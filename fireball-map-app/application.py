8218be380af12cd450f7a752d2161ced097a35f0
ashv266 <as3735@drexel.edu>
2016-04-18 20:58:22 -0400

    Using KMS Alias key to connect to instances

--- fireball-map-app/application.py
+++ fireball-map-app/application.py
@@ -1,9 +1,11 @@
 from flask import Flask
+ 
 application = Flask(__name__)
+keyAlias = "alias/testKMSKey"
 
-@application.route('/')
+@application.route('/CheckKeyName')
 def hello_world():
-    return 'Hello World! This is Ash and the Beanstalk.'
+    return 'Hello World! This is Ash and the Beanstalk. Here is the key: '+keyAlias
 
 if __name__ == '__main__':
     application.debug = True
